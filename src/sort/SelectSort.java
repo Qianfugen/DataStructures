80b1c009501cf1cca61610b7b886196c42d7d921
qianfg <15708915671@163.com>
2020-05-02 15:37:54 +0800

    选择排序

--- src/sort/SelectSort.java
+++ src/sort/SelectSort.java
@@ -1,15 +1,36 @@
 package sort;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * 选择排序
  */
 public class SelectSort {
     public static void main(String[] args) {
-        int[] arr = {1, 34, 119, 101};
+//        int[] arr = {1, 34, 119, 101};
+//        selectSort(arr);
+//        System.out.println("排序后: " + Arrays.toString(arr));
+
+        //测试80000数据
+        int[] arr = new int[80000];
+        for (int i = 0; i < arr.length; i++) {
+            //产生[0,80000)的随机数,填充数组
+            arr[i] = (int) (Math.random() * 80000);
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date1 = new Date();
+        String date1Str = sdf.format(date1);
+        System.out.println("排序前时间: " + date1Str);
+
+        //开始排序
         selectSort(arr);
-        System.out.println("排序后: " + Arrays.toString(arr));
+
+        Date date2 = new Date();
+        String date2Str = sdf.format(date2);
+        System.out.println("排序后时间: " + date2Str);
     }
 
     public static void selectSort(int[] arr) {
