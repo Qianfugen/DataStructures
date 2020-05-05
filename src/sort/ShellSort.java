dde81a8c09d451bcd3519aa2348a16440497b7d9
qianfg <15708915671@163.com>
2020-05-05 16:00:08 +0800

    希尔排序

---
+++ src/sort/ShellSort.java
@@ -0,0 +1,53 @@
+package sort;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+/**
+ * 希尔排序
+ */
+public class ShellSort {
+    public static void main(String[] args) {
+//        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+//        shellSort(arr);
+
+        //测试80000数据
+        int[] arr = new int[80000];
+        for (int i = 0; i < arr.length; i++) {
+            //产生[0,80000)的随机数,填充数组
+            arr[i] = (int) (Math.random() * 80000);
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date1 = new Date();
+        String date1Str = sdf.format(date1);
+        System.out.println("排序前时间: " + date1Str);
+
+        //开始排序
+        shellSort(arr);
+
+        Date date2 = new Date();
+        String date2Str = sdf.format(date2);
+        System.out.println("排序后时间: " + date2Str);
+    }
+
+    public static void shellSort(int[] arr) {
+        int temp = 0;
+        int count = 0;
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                for (int j = i - gap; j >= 0; j -= gap) {
+                    if (arr[j] > arr[j + gap]) {
+                        temp = arr[j];
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = temp;
+                    }
+                }
+            }
+//            System.out.printf("希尔排序第%d趟: %s\n", ++count, Arrays.toString(arr));
+        }
+    }
+
+
+}
